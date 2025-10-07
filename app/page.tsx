e91116cfe743ca9476e387679dc0f09aa80cd39d
cuerojr <cannibal5033@gmail.com>
2025-10-07 15:09:43 -0300

    Hidded enterprise section

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
     <div className="min-h-screen bg-background">
       <HeroSection />
       <FeaturesSection />
-      <EnterpriseSection />
+      {/*<EnterpriseSection />*/}
       <TripticoSection />
       <Footer />
     </div >
