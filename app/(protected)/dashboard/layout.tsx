0a9e1a4425186189f146a4474b730af3c563c5d4
uni-delphi <universidad.delphi@gmail.com>
2025-12-09 13:20:49 -0300

    pasando props dentro de mnenu

--- app/(protected)/dashboard/layout.tsx
+++ app/(protected)/dashboard/layout.tsx
@@ -1,15 +1,10 @@
-"use client"
-
-import AdminDropDown from "@/components/admin-dropdown/admin-dropdown";
 import { Menu } from "@/components/menu";
-import { useState } from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [activeSection, setActiveSection] = useState("perfil");
 
 
   return (
@@ -18,8 +13,7 @@ export default function RootLayout({
         <div className="grid grid-cols-12 gap-4 min-h-screen">
           <div className="col-span-12 md:col-span-1 border-black h-full">
             <Menu
-              activeSection={activeSection}
-              onSectionChange={setActiveSection}
+
             />
           </div>
           <div className="col-span-12 md:col-span-11">{children}</div>
--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -1,20 +1,23 @@
 "use client";
-
+import { useState } from "react";
 import { useIsMobile } from "@/app/hooks/use-is-mobile";
 import { Sidebar } from "../sidebar/sidebar";
 import { MobileMenu } from "../mobile-menu";
 
-interface MenuProps {
-    activeSection?: string;
-    onSectionChange?: (section: string) => void;
-}
 
-export function Menu(props: MenuProps) {
+export function Menu() {
+    const [activeSection, setActiveSection] = useState("perfil");
     const isMobile = useIsMobile();
 
     if (isMobile) {
-        return <MobileMenu {...props} />;
+        return <MobileMenu
+            activeSection={activeSection}
+            onSectionChange={setActiveSection}
+        />;
     }
 
-    return <Sidebar {...props} />;
+    return <Sidebar
+        activeSection={activeSection}
+        onSectionChange={setActiveSection}
+    />;
 }
